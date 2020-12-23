fe3cbc158641df5c01d08cad861818c599d65265
SofiaRodcevich <rodcevich.sofia1@gmail.com>
2020-12-23 22:15:18 +0200

    Task_1_Rodcevich

---
+++ app/Http/Controllers/File1Controller.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\file_1;
+use Illuminate\Http\Request;
+
+class File1Controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\file_1  $file_1
+     * @return \Illuminate\Http\Response
+     */
+    public function show(file_1 $file_1)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\file_1  $file_1
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(file_1 $file_1)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\file_1  $file_1
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, file_1 $file_1)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\file_1  $file_1
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(file_1 $file_1)
+    {
+        //
+    }
+}
---
+++ app/Models/file_1.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class file_1 extends Model
+{
+    protected $fillable = [
+      'name'
+    ];
+}
